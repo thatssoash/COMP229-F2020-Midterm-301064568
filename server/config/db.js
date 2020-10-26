bf63fc97def3e6c1a3693d597760b2c22682e819
Asheka Hall <thatssoash@hotmail.com>
2020-10-26 17:46:52 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://Admin:admin@cluster0.kk4xb.mongodb.net/book_store?retryWrites=true&w=majority"
+};
